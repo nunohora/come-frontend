2c8665b00f68518d7c89ce13453c3f9949e3968c
Nuno Hora <nunohora@gmail.com>
2015-07-24 19:28:25 +0100

    more userstore testing

--- src/__tests__/stores/UserStore-test.js
+++ src/__tests__/stores/UserStore-test.js
@@ -6,17 +6,39 @@ jest.dontMock('../../js/stores/UserStore');
 jest.dontMock('../../js/constants/Constants');
 
 describe('UserStore', function() {
-    var UserStore, AppDispatcher, CONSTANTS, callback;
+    var CONSTANTS = require('../../js/constants/Constants'),
+        UserStore, AppDispatcher, callback;
+
+    // mock actions inside dispatch payloads
+    var actionUserLogin= {
+        source: 'VIEW_ACTION',
+        action: {
+            actionType: CONSTANTS.USER_LOGIN,
+            text: 'foo'
+        }
+    };
+
+    var actionUserLogout = {
+        source: 'VIEW_ACTION',
+        action: {
+            actionType: CONSTANTS.USER_LOGOUT,
+            id: 'replace me in test'
+        }
+    };
 
     beforeEach(function() {
-        CONSTANTS = require('../../js/constants/Constants');
         AppDispatcher = require('../../js/dispatcher/AppDispatcher');
         AppDispatcher.register = jest.genMockFunction();
 
         UserStore = require('../../js/stores/UserStore');
+        callback = AppDispatcher.register.mock.calls[0][0];
     });
 
     it('registers a callback with the dispatcher', function () {
         expect(AppDispatcher.register.mock.calls.length).toBe(1);
+    });
+
+    xit('checks that localStorage has user object after login', function () {
+
     });
 });
