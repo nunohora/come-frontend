9d61a69b6f7aaf4f141d3a34099e9514cd184dec
Nuno Hora <nunohora@gmail.com>
2015-07-24 20:49:41 +0100

    finished testing restaurant list store

---
+++ src/__tests__/stores/RestaurantListStore-test.js
@@ -0,0 +1,61 @@
+"use strict";
+
+jest.autoMockOn();
+
+jest.dontMock('../../js/stores/RestaurantListStore');
+jest.dontMock('../../js/constants/Constants');
+
+describe('RestaurantListStore', function() {
+    var CONSTANTS = require('../../js/constants/Constants'),
+        mockData = require('../mockData/restaurantList.json'),
+        RestaurantListStore, AppDispatcher, callback;
+
+    // mock actions inside dispatch payloads
+    var actionGetRestListStore = {
+        actionType: CONSTANTS.GET_REST_LIST,
+        params: '4050-234',
+        response: mockData
+    };
+
+    beforeEach(function() {
+        AppDispatcher = require('../../js/dispatcher/AppDispatcher');
+        AppDispatcher.register = jest.genMockFunction();
+        RestaurantListStore = require('../../js/stores/RestaurantListStore');
+        callback = AppDispatcher.register.mock.calls[0][0];
+    });
+
+    it('registers a callback with the dispatcher', function () {
+        expect(AppDispatcher.register.mock.calls.length).toBe(1);
+    });
+
+    it('checks right categories are returned', function () {
+        var categories;
+
+        callback(actionGetRestListStore);
+
+        categories = RestaurantListStore.getCategories();
+
+        expect(categories.length).toBe(5);
+        expect(categories[0].name).toBe('Total');
+        expect(categories[0].resultNumber).toBe(4);
+
+        expect(categories[2].name).toBe('Chinesa');
+        expect(categories[2].resultNumber).toBe(2);
+    });
+
+    it('checks if filtering by category returns the right results', function () {
+        var filtered;
+
+        callback(actionGetRestListStore);
+
+        filtered = RestaurantListStore.filterByCategory();
+
+        expect(filtered.length).toBe(4);
+        expect(filtered[0].name).toBe('Torphy-Schoen 378');
+
+        filtered = RestaurantListStore.filterByCategory(2);
+
+        expect(filtered.length).toBe(2);
+        expect(filtered[0].name).toBe('Hilpert-Hoppe 494');
+    });
+});
